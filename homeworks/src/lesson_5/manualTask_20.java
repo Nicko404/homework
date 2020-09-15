ad89f86efd4368a390da63d6d6728a84d09292f8
Kolya Sakovich <kolsak2013@yandex.ru>
2020-09-15 12:54:15 +0300

    gsd

--- homeworks/src/lesson_5/manualTask_20.java
+++ homeworks/src/lesson_5/manualTask_20.java
@@ -1,7 +1,5 @@
 package lesson_5;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import org.w3c.dom.ls.LSOutput;
 
 public class manualTask_20 {
     public static void main(String... args) {
